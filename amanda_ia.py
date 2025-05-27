c6ad8f28ebf43042d095bf0b085303c6926b24e9
ErickGM751 <erickguzman539@gmail.com>
2025-05-27 16:08:00 -0600

    Update amanda_ia.py

--- amanda_ia.py
+++ amanda_ia.py
@@ -56,12 +56,12 @@ SERVICIOS = {
         "nombre": "🎬 Video Personalizado",
         "nombre": "🎬 Video Personalizado",
         "descripcion": """🎬 <b>Video Personalizado</b> — $500 MXN
-🕒 20 minutos haciendo lo que tú digas
+ 20 minutos haciendo lo que tú digas
 🎭 Desinhibida, obediente y entregada
 📦 Entrega < 12 hrs
 🎁 Incluye 15 días <b>GRATIS</b> en Canal VIP
 ➡️ Escríbeme: @ami_pra"""
-🕒 20 minutos haciendo lo que tú digas
+ 20 minutos haciendo lo que tú digas
 🎭 Desinhibida, obediente y entregada
 📦 Entrega < 12 hrs
 🎁 Incluye 15 días <b>GRATIS</b> en Canal VIP
